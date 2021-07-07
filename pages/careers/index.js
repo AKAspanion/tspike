d917ce7014b6ec0889fd7a1947af41a17a97470e
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-07-07 13:56:24 +0530

    ✨ Careers page created

---
+++ pages/careers/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+import { Layout, LocationGrid, TitleCard, VideoBanner } from '../../components';
+
+const overline = 'JOIN US.';
+const heading = 'Do you want to be part of the team?';
+
+export default function Careers() {
+  return (
+    <Layout>
+      <Head>
+        <title>Careers</title>
+      </Head>
+      <TitleCard overline={overline} heading={heading} />
+      {/* video in careers section is a viemo blob, can't use it here  */}
+      <VideoBanner url="https://www-cdn.tigerspike.com/wp-content/uploads/2019/10/Homepage-Video-muted.mp4?_=1" />
+
+      <LocationGrid />
+    </Layout>
+  );
+}
